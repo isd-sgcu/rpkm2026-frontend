bd9b444e1cf2318acc06bdd3ce6f318a4b8649fb
Ongsalt <ongsa.srp.mkm@gmail.com>
2026-07-13 23:42:40 +0700

    chore: update sonner styling

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -24,15 +24,15 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
+          "--normal-bg": "var(--background)",
+          "--normal-text": "var(--foreground)",
           "--normal-border": "var(--border)",
           "--border-radius": "var(--radius)",
         } as React.CSSProperties
       }
       toastOptions={{
         classNames: {
-          toast: "cn-toast",
+          toast: "cn-toast font-sans",
         },
       }}
       {...props}
