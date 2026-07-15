9df4c1153215d1597694bdc9481dbdfe4ab83e5a
Weerawit Pungpiboon <weerawitweem@gmail.com>
2026-07-15 20:01:27 +0700

    feat: ranking house component

--- src/components/house/HouseSelector.tsx
+++ src/components/house/HouseSelector.tsx
@@ -3,34 +3,96 @@ import selectHouseIcon from "@assets/icons/house_select.svg";
 import type { RankingHouses } from "./Ranking";
 // import { useState } from "react";
 import { HOUSES } from "../../consts/house";
+import house_rank_1 from "@assets/images/house/house_rank_1.svg";
+import house_rank_2 from "@assets/images/house/house_rank_2.svg";
+import house_rank_3 from "@assets/images/house/house_rank_3.svg";
+import house_rank_4 from "@assets/images/house/house_rank_4.svg";
+import house_rank_5 from "@assets/images/house/house_rank_5.svg";
+import house_delete from "@assets/icons/delete.svg";
+import danger_icon from "@assets/icons/danger.svg";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@components/ui/alert-dialog";
+import { useT } from "@lib/i18n/useT";
 
 type HouseSelectorProps = {
   rank: keyof RankingHouses;
   index: number;
   selectedHouses: RankingHouses;
+  isEditing?: boolean;
   onChangeHouse: (rank: keyof RankingHouses) => void;
+  onDeleteHouse: (rank: keyof RankingHouses) => void;
 };
 
 export default function HouseSelector({
   rank,
   index,
   selectedHouses,
+  isEditing = false,
   onChangeHouse,
+  onDeleteHouse,
 }: HouseSelectorProps) {
+  const t = useT();
   //   const [selectedHouse, setSelectedHouse] = useState<string | null>(
   //     selectedHouses[rank],
   //   );
+
   const houseData = HOUSES.find(
     (house) => house.name.th === selectedHouses[rank],
   );
   const size = houseData?.size.toLowerCase();
+  const logo = houseData?.logoUrl;
+
+  const house_rank_images = [
+    house_rank_1,
+    house_rank_2,
+    house_rank_3,
+    house_rank_4,
+    house_rank_5,
+  ];
+  const house_rank_image = house_rank_images[index];
 
   return (
     <div
       key={rank}
-      className="flex items-center justify-center bg-white border border-black rounded-full p-3"
-      onClick={() => onChangeHouse(rank)}
+      className="relative flex w-[90%] items-center justify-center bg-white border border-black rounded-full p-3"
+      onClick={() => {
+        if (!selectedHouses[rank]) {
+          onChangeHouse(rank);
+        }
+      }}
     >
+      <div
+        className={`absolute top-0 -left-2.5 h-20 w-20 ${
+          !selectedHouses[rank] ? "grayscale" : ""
+        }`}
+      >
+        <img
+          src={house_rank_image.src}
+          alt={`House Rank ${index + 1}`}
+          className="absolute -top-2.5 -left-2.5 z-20"
+        />
+
+        <div className="w-full h-full bg-rpkm-blue border rounded-xl flex items-center justify-center origin-top-left -rotate-10">
+          {logo ? (
+            <img
+              src={logo}
+              alt={`${selectedHouses[rank]} logo`}
+              className="w-[90%] h-[90%] bg-white object-contain rounded-lg border z-10"
+            />
+          ) : (
+            <div className="w-[90%] h-[90%] bg-gray-300 rounded-lg border" />
+          )}
+        </div>
+      </div>
       {selectedHouses[rank] ? (
         <div className="flex flex-col items-center">
           <h2
@@ -51,6 +113,54 @@ export default function HouseSelector({
           alt="select house"
           className="w-12 h-12"
         />
+      )}
+      {isEditing && selectedHouses[rank] && (
+        <AlertDialog>
+          <AlertDialogTrigger
+            render={
+              <button
+                type="button"
+                className="absolute top-1/2 right-8 -translate-y-1/2 z-30"
+                onClick={(e) => e.stopPropagation()}
+              />
+            }
+          >
+            <img
+              src={house_delete.src}
+              alt="Delete house"
+              className="w-6 h-6"
+            />
+          </AlertDialogTrigger>
+
+          <AlertDialogContent className="pt-10">
+            <AlertDialogHeader>
+              <AlertDialogMedia className="absolute top-0 translate-y-[-50%] bg-red-500 w-full border py-2 h-fit rounded-2xl">
+                <img src={danger_icon.src} alt="danger" />
+              </AlertDialogMedia>
+
+              <AlertDialogTitle>
+                {t("house.ranking.houseRemove")}
+              </AlertDialogTitle>
+
+              <AlertDialogDescription className={"whitespace-break-spaces"}>
+                {t("house.ranking.houseRemoveDesc", {
+                  House: selectedHouses[rank],
+                })}
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <div className="flex gap-4 justify-center">
+              <AlertDialogAction
+                className="bg-red-500 text-white"
+                onClick={() => onDeleteHouse(rank)}
+              >
+                Confirm
+              </AlertDialogAction>
+
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+            </div>
+          </AlertDialogContent>
+        </AlertDialog>
       )}
     </div>
   );
--- src/components/house/Ranking.tsx
+++ src/components/house/Ranking.tsx
@@ -2,9 +2,23 @@ import { MonotoneNoiseContainer } from "@components/shared/MonotoneNoise";
 import { useState } from "react";
 import { useT } from "@lib/i18n/useT";
 import { Button } from "@components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@components/ui/alert-dialog";
 import stamp from "@assets/images/house/house_ranking_stamp.svg";
 import HouseSelector from "./HouseSelector";
 import HouseSelectPopup from "./HouseSelectPopup";
+import edit_icon from "@assets/icons/edit.svg";
+import danger_icon from "@assets/icons/danger.svg";
+import success_icon from "@assets/icons/success.svg";
 
 export type RankingHouses = {
   house1: string | null;
@@ -19,14 +33,6 @@ export type RankingProps = {
 };
 
 export default function Ranking() {
-  const [haveSelectedHouse, setHaveSelectedHouse] = useState<boolean>(false);
-  const [selectedHouses, setSelectedHouses] = useState<RankingHouses>({
-    house1: null,
-    house2: null,
-    house3: null,
-    house4: null,
-    house5: null,
-  });
   const rankingKeys: (keyof RankingHouses)[] = [
     "house1",
     "house2",
@@ -37,15 +43,80 @@ export default function Ranking() {
   const [activeRank, setActiveRank] = useState<keyof RankingHouses | null>(
     null,
   );
+  const [haveSelectedHouse, setHaveSelectedHouse] = useState<boolean>(false);
+  const [selectedHouses, setSelectedHouses] = useState<RankingHouses>({
+    house1: null,
+    house2: null,
+    house3: null,
+    house4: null,
+    house5: null,
+  });
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [showClearConfirm, setShowClearConfirm] = useState<boolean>(false);
+  const [showSaveAlert, setShowSaveAlert] = useState(false);
+  const [saveAlertType, setSaveAlertType] = useState<"error" | "success">(
+    "error",
+  );
+
   const t = useT();
+  const hasSelectedHouse = Object.values(selectedHouses).some(
+    (house) => house !== null,
+  );
+  const canEdit = hasSelectedHouse && isEditing;
+
+  const handleDeleteHouse = (rank: keyof RankingHouses) => {
+    setSelectedHouses((prev) => ({
+      ...prev,
+      [rank]: null,
+    }));
+  };
 
   const handleOpenSelector = (rank: keyof RankingHouses) => {
     setActiveRank(rank);
   };
 
+  const handleSelectHouse = (house: string) => {
+    if (!activeRank) return;
+
+    setSelectedHouses((prev) => {
+      const updated = { ...prev };
+
+      // Remove the house if it already exists in another rank
+      Object.keys(updated).forEach((rank) => {
+        const key = rank as keyof RankingHouses;
+
+        if (updated[key] === house) {
+          updated[key] = null;
+        }
+      });
+
+      // Assign the house to the selected rank
+      updated[activeRank] = house;
+
+      return updated;
+    });
+
+    setActiveRank(null);
+  };
+
   const handleSubmit = () => {
-    // Handle the submission logic here
-    console.log("Submit button clicked");
+    const hasEmptyRank = Object.values(selectedHouses).some(
+      (house) => house === null,
+    );
+
+    if (hasEmptyRank) {
+      setSaveAlertType("error");
+      setShowSaveAlert(true);
+      return;
+    }
+
+    // Mock API
+    console.log("Mock API payload:", selectedHouses);
+
+    setTimeout(() => {
+      setSaveAlertType("success");
+      setShowSaveAlert(true);
+    }, 500);
   };
 
   return (
@@ -56,20 +127,89 @@ export default function Ranking() {
         className="absolute top-0 left-0 transform translate-y-[-60%] z-10 mx-auto mt-4"
       />
       <MonotoneNoiseContainer className="w-full bg-rpkm-red rounded-4xl border p-4 py-6">
-        <h1 className="text-white font-bold text-2xl text-center">
-          {t("house.ranking.title")}
-        </h1>
+        <div className="relative">
+          <h1 className="text-white font-bold text-2xl text-center">
+            {t("house.ranking.title")}
+          </h1>
+
+          {hasSelectedHouse &&
+            (canEdit ? (
+              <AlertDialog
+                open={showClearConfirm}
+                onOpenChange={setShowClearConfirm}
+              >
+                <AlertDialogTrigger
+                  render={
+                    <Button
+                      type="reset"
+                      variant="link"
+                      size="lg"
+                      className="absolute top-0 right-0 bg-white text-rpkm-red p-2 rounded-full w-fit aspect-square"
+                    />
+                  }
+                >
+                  {t("house.ranking.houseClear")}
+                </AlertDialogTrigger>
+                <AlertDialogContent className={"pt-10"}>
+                  <AlertDialogHeader>
+                    <AlertDialogMedia className="absolute top-0 translate-y-[-50%] bg-red-500 w-full border py-2 h-fit rounded-2xl">
+                      <img src={danger_icon.src} alt="danger" />
+                    </AlertDialogMedia>
+                    <AlertDialogTitle className="text-xl text-black font-bold">
+                      {t("house.ranking.houseClearTitle")}
+                    </AlertDialogTitle>
+                    <AlertDialogDescription className="text-lg text-black">
+                      {t("house.ranking.houseClearDescription")}
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <div className="flex gap-4 justify-center">
+                    <AlertDialogAction
+                      className="bg-red-500 text-white"
+                      onClick={() => {
+                        setSelectedHouses({
+                          house1: null,
+                          house2: null,
+                          house3: null,
+                          house4: null,
+                          house5: null,
+                        });
+                        setIsEditing(false);
+                        setShowClearConfirm(false);
+                      }}
+                    >
+                      {t("walkrally.events.confirm") || "Confirm"}
+                    </AlertDialogAction>
+                    <AlertDialogCancel>
+                      {t("walkrally.events.cancel") || "Cancel"}
+                    </AlertDialogCancel>
+                  </div>
+                </AlertDialogContent>
+              </AlertDialog>
+            ) : (
+              <Button
+                type="button"
+                size="lg"
+                className="absolute top-0 right-0 bg-white p-2 rounded-full w-fit aspect-square"
+                onClick={() => setIsEditing(true)}
+              >
+                <img src={edit_icon.src} alt="edit" className="w-full h-full" />
+              </Button>
+            ))}
+        </div>
+
         {haveSelectedHouse ? (
           <div className="flex flex-col items-center gap-5 mt-6">
             {/* // map through the houses and display them */}
-            <div className="flex flex-col gap-3 w-full">
+            <div className="flex flex-col justify-center items-center gap-3 w-full">
               {rankingKeys.map((rank, index) => (
                 <HouseSelector
                   key={rank}
                   rank={rank}
                   index={index}
                   selectedHouses={selectedHouses}
+                  isEditing={canEdit}
                   onChangeHouse={handleOpenSelector}
+                  onDeleteHouse={handleDeleteHouse}
                 />
               ))}
             </div>
@@ -84,6 +224,7 @@ export default function Ranking() {
             </div>
           </div>
         ) : (
+          // if the user has not selected a house, show a message and a button to select a house
           <div className="flex flex-col items-center gap-4 mt-6">
             <p className="text-rpkm-yellow text-3xl font-normal text-center whitespace-pre-line">
               {t("house.ranking.noHouses")}
@@ -103,16 +244,53 @@ export default function Ranking() {
       {activeRank && (
         <HouseSelectPopup
           onClose={() => setActiveRank(null)}
-          onSelect={(house: string) => {
-            setSelectedHouses((prev) => ({
-              ...prev,
-              [activeRank]: house,
-            }));
-
-            setActiveRank(null);
-          }}
+          onSelect={handleSelectHouse}
         />
       )}
+
+      <AlertDialog open={showSaveAlert} onOpenChange={setShowSaveAlert}>
+        <AlertDialogContent className="pt-10">
+          <AlertDialogHeader>
+            <AlertDialogMedia
+              className={`absolute top-0 translate-y-[-50%] w-full border py-2 h-fit rounded-2xl ${
+                saveAlertType === "success" ? "bg-green-500" : "bg-rpkm-red"
+              }`}
+            >
+              <img
+                src={
+                  saveAlertType === "success"
+                    ? success_icon.src
+                    : danger_icon.src
+                }
+                alt={saveAlertType}
+              />
+            </AlertDialogMedia>
+
+            <AlertDialogTitle className="text-xl text-black font-bold">
+              {saveAlertType === "error"
+                ? t("house.ranking.saveIncompleteTitle")
+                : t("house.ranking.saveSuccessTitle")}
+            </AlertDialogTitle>
+
+            <AlertDialogDescription className="text-lg text-black">
+              {saveAlertType === "error"
+                ? t("house.ranking.saveIncompleteDescription")
+                : t("house.ranking.saveSuccessDescription")}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <div className="flex justify-center">
+            <AlertDialogAction
+              className={`text-white ${
+                saveAlertType === "success" ? "bg-green-500" : "bg-rpkm-red"
+              }`}
+              onClick={() => setShowSaveAlert(false)}
+            >
+              {t("walkrally.events.confirm") || "Confirm"}
+            </AlertDialogAction>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
